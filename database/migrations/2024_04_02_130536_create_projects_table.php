ed41eb392f2fba57c4dac8df33c0d3b04ba0a6d4
sisdream <sissipascale2@gmail.com>
2024-04-02 18:17:56 +0200

    feat: migrate aggiustate

--- database/migrations/2024_04_02_130536_create_projects_table.php
+++ database/migrations/2024_04_02_130536_create_projects_table.php
@@ -15,10 +15,13 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 50);
+            $table->dateTime('date')->nullable();
+            $table->text('description')->nullable();
+            $table->text('image')->nullable();
             $table->timestamps();
         });
     }
-
     /**
      * Reverse the migrations.
      *
